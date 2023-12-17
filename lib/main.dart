f5983bf8d43b6998e166eff4781f9a3215ed3e87
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-17 12:03:04 -0500

    candidate page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:conectionfirebase2/home_page.dart';
+import 'package:conectionfirebase2/pages/home_page.dart';
+import 'package:conectionfirebase2/pages/home_page2.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -9,6 +10,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MaterialApp(
-    home: HomePage(),
+    home: HomePage2(),
   ));
 }
---
+++ lib/pages/home_page2.dart
@@ -0,0 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage2 extends StatefulWidget {
+  @override
+  State<HomePage2> createState() => _HomePage2State();
+}
+
+class _HomePage2State extends State<HomePage2> {
+  CollectionReference usersReference =
+      FirebaseFirestore.instance.collection('USERS');
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "FirebaseApp",
+          ),
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+          future: usersReference.get(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              QuerySnapshot userCollection = snapshot.data;
+              List<QueryDocumentSnapshot> docs = userCollection.docs;
+              return ListView.builder(
+                itemCount: docs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return miCard();
+                },
+              );
+              // return miCard();
+            } else {
+              return Center(
+                child: Text("error"),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Card miCard() {
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    margin: EdgeInsets.all(15),
+    elevation: 10,
+    child: Row(
+      children: [
+        Column(
+          children: <Widget>[
+            Column(
+              children: [
+                Container(
+                  width: 300,
+                  child: ListTile(
+                    contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                    title: Text("Candidato"),
+                    subtitle: Text('Por la Ciudad de Lima'),
+                    leading: Icon(Icons.home),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text('Votar'),
+          ],
+        )
+      ],
+    ),
+  );
+}
